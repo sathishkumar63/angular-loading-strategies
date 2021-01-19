f3d9868eff3af45785a0a7796aaa4a8d1e06fa3e
sathishkumar63 <sathishjim@gmail.com>
2021-01-19 14:49:41 +0530

    add custom pre loading strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CustomPreloadingStrategy } from './core/custom-preloading-strategy';
 
 const routes: Routes = [
+  // { path: '', redirectTo: 'lazy-loading', pathMatch: 'full' },
   {
     path: 'lazy-loading',
     loadChildren: () =>
       import('./features/lazy-loading/lazy-loading.module').then(
         (m) => m.LazyLoadingModule
-      ),
+      )
   },
   {
     path: 'pre-loading',
@@ -15,11 +17,16 @@ const routes: Routes = [
       import('./features/pre-loading/pre-loading.module').then(
         (m) => m.PreLoadingModule
       ),
+    data: { preload: true } // Preload only this module
   },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      preloadingStrategy: CustomPreloadingStrategy,
+    }),
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
