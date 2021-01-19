fbeddad44bece308914bf20589a8b92e643f5a4f
sathishkumar63 <sathishjim@gmail.com>
2021-01-19 16:22:03 +0530

    add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomPreloadingStrategy } from './core/custom-preloading-strategy';
 import { EagerLoadingModule } from './features/eager-loading';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, EagerLoadingModule],
+  imports: [BrowserModule, AppRoutingModule, EagerLoadingModule, BrowserAnimationsModule],
   providers: [CustomPreloadingStrategy],
   bootstrap: [AppComponent],
 })
